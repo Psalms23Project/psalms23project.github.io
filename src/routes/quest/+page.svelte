4dff8f7c662b45a4f9bd0177026fa1c3419a1392
Noah Rahm <correctsyntax@yahoo.com>
2024-01-28 18:06:43 -0600

    Add other pages

---
+++ src/routes/quest/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+
+</script>
+
+<svelte:head>
+  <title>The Quest for Quiet | The Psalms 23 Project</title>
+  <meta name="description" content="The Psalms 23 Project aims to create high-quality videos of Psalm readings on location, in nature with the peaceful atmosphere and sounds of creation." />
+</svelte:head>
+
+<div class="max-w-6xl mx-auto px-4 my-20">
+  <div class="max-w-3xl font-charter text-navyblue">
+    <h2 class="text-4xl sm:text-5xl font-hightower mt-4">The Quest for Quiet</h2>
+    <h3 class="text-2xl font-hightower mt-2">A call for quiet locations to film readings of the Psalms in nature.</h3>
+    <p class="text-2xl font-hightower mt-4">Why we need quiet locations</p>
+    <p class="text-lg mt-2">The Psalms 23 project aims to film high-quality readings of the Psalms in nature, with the peaceful atmosphere and sounds of creation.</p>
+    <p class="text-lg mt-2">Most Scripture and Psalm readings that you find online are recorded in a sound-proof studio. Nature soundtracks and foley (sound effects) are mixed in digitally after recording.</p>
+    <p class="text-lg mt-2">For the Psalms 23 project we are recording natural sounds and filming the reading in real time, on location, to preserve the full atmosphere of each place. Because of this, the locations where we film and record needs to be quiet, away from unnatural sounds. 
+    <p class="text-2xl font-hightower mt-4">Natural vs. Unnatural sounds</p>
+    <p class="text-lg mt-2">When we refer to "unnatural sounds", generally, we mean sounds in the background that would distract someone from listening to and enjoying the Psalm readings. This includes road noise, people talking, machinery, dogs barking, electronic buzz, and airplanes. Acceptable natural sounds would be: birds, normal insect noise, and farm animals (as long as they aren't too noisy).</p>
+    <p class="text-lg mt-2">This rules out recording most anywhere near busy roads, places with any kind of large groups of people, and even some private property with noisy neighbors. Lawnmowers, noises from neighbors, or vehicles running nearby would delay or prevent us from recording. </p>
+    <p class="text-lg mt-2">Finding truly quiet places where only natural sounds can be heard for even a few minutes (let alone a few hours) is a difficult order. It's not until you try to do a project like this that you realize just how difficult it is to get away from unnatural noise in general.</p>
+    <p class="text-lg mt-2">How do we find locations that are quiet enough and match the setting that we need for each particular Psalm?</p>
+    <p class="text-lg mt-2">We need your help!</p>
+    <p class="text-2xl font-hightower mt-4">Would you like to share your location for us to use?</p>
+    <p class="text-lg mt-2">Would you like to help us make the Psalms 23 Project possible by allowing us to film on your property? Or, do you know someone else that may have a location we could use? Here are a few things to know:</p>
+    <ul class="list-disc list-inside text-lg mt-2">
+      <li>Since this is a no/low budget project, <b>we cannot afford to pay for our use of your property</b>. That said, we will always treat you and your property with respect. If you'd like to help out or watch as we film the reading(s) you're welcome to.</li>
+      <li>We want to be completely upfront with you on what we expect from you and what you should expect from us. So before filming on your property, <b>we will ask you (the property owner) to sign a Location Release form</b> to grant us the right to 1) bring the necessary equipment on to your property to film 2) to film the Psalm reading(s) on your property, and 3) to display (or choose not to display) the final Psalm reading(s) that were filmed on your property in any form we choose.</li>
+      <li>We will ask that you <b>not run your lawn mower or other noisy machinery, if it can be avoided, on the day(s) that we film</b>. This shouldn't be a problem as long as you realize not to schedule mowing your lawn, etc. on the day(s) we're filming. :)</li>
+      <li>Including setup, <b>each Psalm reading takes between 1-5 hours to film</b>. Depending on how many Psalm readings we agree to film on your property and the weather conditions, <b>you can expect us to need access to your property for 1-14 days</b>. Also, if the filming locations are fairly remote and the time of day for the reading warrants it, we may decide that tent camping overnight is the best option. e.g: for a Psalm reading that is planned for an early morning sunrise, it may be easier for us to camp at the filming location overnight.</li>
+      <li>Because the readings are set to a particular location and weather, we likely need to plan ahead and be ready to film at a moments notice. Most of the time though, we will want to scout out the location beforehand and make sure of each precise setting.</li>
+    </ul>
+    <p class="text-2xl font-hightower mt-4">Location requirements / The ideal location</p>
+    <p class="text-lg mt-2">The ideal location is private country property with suitable places to film at least a good distance away from a highway, busy road, train, or airport. Must be in the continental United States.</p> 
+    <p class="text-lg mt-2">
+      We want to be dilligent to know whether a location is suitable ahead of time so we don't waste anyone's time. So, when submitting your location, please include pictures and videos of places on your property that you think could possibly be used. If you're not sure if your location is quiet enough for what we need, you can still feel free to submit.
+    </p>
+    <p class="text-2xl font-hightower mt-4">What locations are we in need of?</p>
+    <ul class="list-disc list-inside text-lg mt-2">
+      <li><b>Mountain</b> </li>
+      <li><b>Mountain Top</b></li>
+      <li><b>Grassy Meadow/Plain</b></li>
+      <li><b>Stream</b></li>
+      <li><b>River</b></li>
+      <li><b>Lake</b></li>
+      <li><b>Woods/Forest</b></li>
+      <li><b>Waterfall</b></li>
+      <li><b>Hill</b></li>
+      <li><b>Ocean</b></li>
+      <li><b>Desert</b></li>
+      <li><b>Still water by a tree</b></li>
+    </ul>
+    <p class="text-lg mt-4"></p>
+    <p class="text-2xl font-hightower mt-4">How to submit your potential location</p>
+    <p class="text-lg mt-2">
+      Please email us with your hello and pictures/videos of places that you think could possibly be used on your property at <a class="underline" href="mailto:contact@familyfortruth.com">contact@familyfortruth.com</a>
+    </p>
+    <p class="text-lg mt-2">Once you email us, we'll get back to you whether or not we think it would be suitable and quiet enough for filming Psalm readings.</p>
+    <p class="text-lg mt-2">We look forward to hearing from you!</p>
+  </div>
+</div>
