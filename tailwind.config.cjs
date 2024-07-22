e63b5353417850aca565662ed0a9ac833808d989
Noah Rahm <correctsyntax@yahoo.com>
2024-07-22 15:06:32 -0500

    Update website for Psalm 99

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ const config = {
       },
       backgroundImage: {
         'birch-tree': "url('/images/birch-tree.jpg')",
-        'featured-psalm': "url('/images/covers/psalm-149.jpg')",
+        'featured-psalm': "url('/images/covers/psalm-99.jpg')",
         'about': "url('/images/covers/psalm-49.jpg')",
       }
     },
