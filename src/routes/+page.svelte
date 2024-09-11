67cd62ec5f409e1f367672d10a53eb038724e28f
Noah Rahm <correctsyntax@yahoo.com>
2024-09-11 02:18:21 -0500

    Update featured psalm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,13 +14,13 @@
       <p class="text-white text-sm uppercase">Latest Video</p>
       <div class="mt-20">
         <h1 class="text-5xl lg:text-6xl font-serif font-medium text-white mt-5">
-          Psalm 114
+          Psalm 134
         </h1>
         <p class="font-secondary italic text-white/90 text-xl mt-2 max-w-sm leading-6">
-          "The mountains skipped like rams"
+          "Bless ye Yahweh, all ye servants of Yahweh"
         </p>
         <div class="flex mt-10">
-          <a href="/readings/114/" class=" text-navybluedark font-semibold rounded-lg px-5 py-3 bg-white hover:bg-white/80 duration-300">
+          <a href="/readings/134/" class=" text-navybluedark font-semibold rounded-lg px-5 py-3 bg-white hover:bg-white/80 duration-300">
             Watch Video
           </a>
         </div>
