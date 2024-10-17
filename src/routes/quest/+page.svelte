e43e9b89572a7880e3dc84c428bcb152ece57181
Noah Rahm <correctsyntax@yahoo.com>
2024-10-16 21:51:37 -0400

    Update the quest for locations page

--- src/routes/quest/+page.svelte
+++ src/routes/quest/+page.svelte
@@ -3,32 +3,32 @@
 </script>
 
 <svelte:head>
-  <title>The Quest for Quiet | The Psalms 23 Project</title>
+  <title>Our Quest for Quiet Locations | The Psalms 23 Project</title>
   <meta name="description" content="The Psalms 23 Project aims to create high-quality videos of Psalm readings on location, in nature with the peaceful atmosphere and sounds of creation." />
 </svelte:head>
 
 <div class="max-w-6xl mx-auto px-4 mt-10 mb-20"> 
   <div class="max-w-3xl text-navyblue">
-    <img class="w-auto rounded-2xl" src="/images/sunset.jpg" alt=""/>
-    <h1 class="text-4xl sm:text-5xl font-serif mt-8">The Quest for Quiet Locations</h1>
-    <h2 class="text-2xl text-navyblue/80 font-serif italic mt-1">A call for quiet locations to film readings of the Psalms in nature.</h2>
-    <p class="text-2xl font-semibold tracking-tight mt-8">Quiet locations</p>
-    <p class="text-lg mt-2">The Psalms 23 project aims to film high-quality readings of the Psalms in nature, with the peaceful sounds of creation.</p>
-    <!-- <p class="text-2xl font-semibold tracking-tight mt-8">Natural vs. Unnatural sounds</p> -->
+    <img class="w-auto rounded-2xl" src="/images/covers/psalm-134.jpg" alt=""/>
+    <h1 class="text-4xl sm:text-5xl font-serif mt-8">Our Quest for Quiet Locations</h1>
+    <h2 class="text-2xl text-navyblue/80 font-serif italic mt-1">A call for locations to film readings of the Psalms in nature.</h2>
+    <p class="text-lg mt-6">The Psalms 23 project aims to film high-quality readings of the Psalms in nature, with the peaceful sounds of creation.</p>
     <p class="text-lg mt-2">For this project it is important that we minimize the amount of unnatural sounds during filming.</p>
     <p class="text-lg mt-2">When we refer to "unnatural sounds", generally, we mean non-nature sounds in the background that would distract someone from listening to and enjoying the Psalm readings.</p>
-    <p class="text-lg mt-2">Unnatural sounds include: loud road noise, people talking, machinery, dogs barking, electronic buzz, and airplanes. Lawnmowers or vehicles running nearby would also delay or prevent us from recording.</p>
+    <p class="text-lg mt-2">Unnatural sounds include: loud road noise, people talking, machinery, dogs barking, electronic buzz, and airplanes. Lawnmowers or vehicles running nearby could also delay or prevent us from recording.</p>
     <p class="text-lg mt-2">Acceptable natural sounds would be: birds, normal insect noise, and farm animals.</p>
+    <p class="text-lg mt-2"><b>If you are not sure if your location is quiet enough for what we need, please still feel free to contact us. We have made locations near busy roads work by filming between the traffic before, so we may be able to make your location work too. :)</b></p>
     <p class="text-2xl font-semibold tracking-tight mt-8">Location requirements / The ideal location</p>
-    <p class="text-lg mt-2">The ideal location is private country property with suitable natural places to film a good distance away from a highway, busy road, train, or airport. Must be in the continental United States (the lower 48).</p> 
-    <p class="text-lg mt-2">
-      When submitting your location, please include pictures and/or videos of places on your property that you think could possibly be used. <b>If you are not sure if the location is quiet enough for what we need, you can still feel free to contact us. :)</b>
-    </p>
+    <p class="text-lg mt-2">The ideal location is private country property with suitable natural places to film a good distance away from a highway, busy road, train, or airport. Must be in the continental United States (the lower 48) or Alaska.</p> 
+    <p class="text-lg mt-2">When submitting your location, please include pictures and/or videos of places on your property that you think could possibly be used. </p>
+    <p class="text-lg mt-2">We're looking for locations on private land since the permitting process to film on state, national, and public lands is often tedious, long, and sometimes expensive. For some areas this is due to the fact that the permitting laws were created when filming always meant a movie production with a huge crew of people. So, even though we're extremely small and not doing anything much more complicated than someone might do with their cellphone and a tripod, our filming is subject to many of the same processes and rules as a large production.</p>
+    <p class="text-lg mt-2">We are aware of the preposed EXPLORE act which passed Congress earlier this year that would, among other things, modernize and ease the permitting process for smaller projects like ours. However, it still needs to pass the Senate and the POTUS to become law. And then it may only affect national parks and may take time for the states to follow suit, if at all. </p>
+    <p class="text-lg mt-2">We are very thankful that Yahweh has provided us with some beautiful locations across different states that we were able to use to film the videos we've completed so far.</p>
     <p class="text-2xl font-semibold tracking-tight mt-8">Your questions answered</p>
     <p class="text-lg mt-2">Would you like to help us make the Psalms 23 Project possible by allowing us to film on your property? Or, do you know someone else that may have a location we could use? Here are some answers to questions you may have:</p>
     <p class="text-xl font-semibold tracking-tight mt-4">Q: What locations are you in need of?</p>
     <p class="text-lg mt-2">A: See what Psalm readings and locations we're planning by <a class="font-semibold underline hover:opacity-90 duration-300 ease-in-out" href="/locations/">clicking here</a>.</p> 
-    <p class="text-xl font-semibold tracking-tight mt-4">Q: What gear do you use? Is it large or a lot of gear?</p>
+    <p class="text-xl font-semibold tracking-tight mt-4">Q: What gear do you use?</p>
     <p class="text-lg mt-2">A: <b>We are a very small production with minimal gear and a small footprint.</b> We <b>don't</b> use external flash, reflectors, stands, lights, or any heavy equipment. Our equipment is small and portable. Except for our regular-size, aluminum tripod, all of our gear that we need for filming can fit inside of a normal school backpack!
     <p class="text-lg mt-2">For those who are interested, the following is the list of our gear: </p>
     <ul class="list-disc list-inside text-lg mt-2">
@@ -45,15 +45,9 @@
     <p class="text-xl font-semibold tracking-tight mt-4">Q: Are you able to compensate for your use of the property?</p>
     <p class="text-lg mt-2">A: <b>Unfortunately, since this is a low budget, not-for-a-profit ministry project, we cannot afford to pay for our use of your property</b>. That said, we will always treat you and your property with respect. If you'd like to help out or watch as we film the reading(s) you are welcome to.</p>
     <p class="text-xl font-semibold tracking-tight mt-4">Q: Will we sign a contract?</p>
-    <p class="text-lg mt-2">A: We certainly can and probably should. <b>We want to be completely upfront with you on what we expect from you and what you should expect from us. If you have any questions or concerns please let us know. Before filming on your property, we may ask you (the property owner) to sign the <a class="font-semibold underline hover:opacity-90 duration-300 ease-in-out" href="/files/location-release.pdf">Location Release form</a></b> to grant us the permissions in writing to 1) bring what we need on to your property to film 2) to film the Psalm reading(s) on your property, and 3) to display (or choose not to display) the final Psalm reading(s) that were filmed on your property in any form we choose.</p>
+    <p class="text-lg mt-2">A: We certainly can if you would like. <b>Either way, we want to be completely upfront with you on what we expect from you and what you should expect from us. If you have any questions or concerns please let us know. Before filming on your property, we may ask you (the property owner) to sign the <a class="font-semibold underline hover:opacity-90 duration-300 ease-in-out" href="/files/location-release.pdf">Location Release form</a></b> to grant us the permissions in writing to 1) bring what we need on to your property to film 2) to film the Psalm reading(s) on your property, and 3) to display (or choose not to display) the final Psalm reading(s) that were filmed on your property in any form we choose.</p>
     <p class="text-xl font-semibold tracking-tight mt-4">Q: Do you have insurance?</p>
-    <p class="text-lg mt-2">A: Yes, our filming is insured under a general liability insurance policy with <a class="font-semibold underline hover:opacity-90 duration-300 ease-in-out" href="https://fullframeinsurance.com">Full Frame Insurance</a>.</p>
-    <p class="text-2xl font-semibold tracking-tight mt-8">Other helpful things to know</p>
-    <ul class="list-disc list-inside text-lg mt-2">
-      <li>If possible, we would ask that you <b>not run your lawn mower or other noisy machinery, if it can be avoided, at the times that we film</b>. This shouldn't be a problem as long as you realize not to schedule mowing your lawn, etc. at the same time we're filming. :)</li>
-      <li>Because the readings are set to particular times of day and weather conditions, we likely need to plan ahead and be ready to film. Most of the time we will want to scout out the location beforehand and make sure of each precise setting.</li>
-      <li><i>(Optional) With your permission, if the filming locations are fairly remote and the time of day for the reading warrants it, tent camping overnight may be the best option. e.g: for a Psalm reading that is planned for an early morning sunrise, it may be easier for us to camp at the filming location overnight.</i></li>
-    </ul>
+    <p class="text-lg mt-2">A: Yes, our filming is insured under a general liability insurance policy with <a class="font-semibold underline hover:opacity-90 duration-300 ease-in-out" href="https://fullframeinsurance.com">Full Frame Insurance</a>. We haven't had any issues in the past, but we want to give you peace of mind should something go awry.</p>
     <p class="text-2xl font-semibold tracking-tight mt-8">How to submit your potential location</p>
     <p class="text-lg mt-2">
       You can either <b>fill out the form below</b> or email us with your hello and pictures/videos of places that you think could possibly be used on your property at <a class="font-semibold underline hover:opacity-90 duration-300 ease-in-out" href="mailto:contact@psalms23project.com">contact@psalms23project.com</a>
