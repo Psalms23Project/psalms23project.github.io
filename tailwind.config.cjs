54baf86014209e76dc2caf1280cc1859d0591737
Noah Rahm <correctsyntax@yahoo.com>
2025-05-14 10:21:23 -0500

    Updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ const config = {
       },
       backgroundImage: {
         'birch-tree': "url('/images/birch-tree.jpg')",
-        'featured-psalm': "url('/images/covers/psalm-105.jpg')",
+        'featured-psalm': "url('/images/covers/psalm-144.jpg')",
         'about': "url('/images/covers/psalm-49.jpg')",
       }
     },
