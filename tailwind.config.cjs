d5e400d149e1fca2cf1913a94e3889a0b8e9ba45
Noah Rahm <correctsyntax@yahoo.com>
2025-11-09 09:43:21 -0600

    Update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ const config = {
       },
       backgroundImage: {
         'birch-tree': "url('/images/birch-tree.jpg')",
-        'featured-psalm': "url('/images/covers/psalm-15.jpg')",
+        'featured-psalm': "url('/images/covers/psalm-122.jpg')",
         'about': "url('/images/covers/psalm-49.jpg')",
       }
     },
